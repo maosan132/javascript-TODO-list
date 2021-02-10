20f8b575d7b055f0a29e5e39da3c1f7eaa51b3ab
maosan132 <maosan132@gmail.com>
2021-02-10 16:29:40 -0500

    chore(TODO): :sparkles: Added html template for rendering projects
    This is the place where the loop with data are gonna render to app

---
+++ src/project-creator.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input');
+const taskList = document.getElementById('task-list')
+const template = document.getElementById('template-task').content
+const fragment = document.createDocumentFragment()
