e4711ad5aacf81243ee6f9339f7e01d2a9536bdd
maosan132 <maosan132@gmail.com>
2021-02-11 14:36:01 -0500

    feat(TODO): :sparkles: Added menu buttons event listener

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,6 +10,6 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-eval("// import files\n\n\n\n//# sourceURL=webpack://javascript-TODO-app/./src/index.js?");
+eval("// import files\n\nconst menu = document.getElementById('menu');\n\n\n\nmenu.addEventListener('click', e => {\n  e.preventDefault();\n  selectActions(e)\n})\n\nconst selectActions = e => {\n  console.log(e.target.id)\n  \n  let menuChoice = e.target.id\n\n  switch (menuChoice) {\n    case 'new':\n      console.log('clicked on','new');\n      newProject();\n      break;\n    case 'default':\n      console.log('clicked on','default');\n      defaultProject();\n      break;\n    case 'projects':\n      console.log('clicked on','projects');\n      RenderProjects()\n      break;\n  }\n}\n\n\n\n//# sourceURL=webpack://javascript-TODO-app/./src/index.js?");
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 // import files
 
+const menu = document.getElementById('menu');
+
+
+
+menu.addEventListener('click', e => {
+  e.preventDefault();
+  selectActions(e)
+})
+
+const selectActions = e => {
+  console.log(e.target.id)
+  
+  let menuChoice = e.target.id
+
+  switch (menuChoice) {
+    case 'new':
+      console.log('clicked on','new');
+      newProject();
+      break;
+    case 'default':
+      console.log('clicked on','default');
+      defaultProject();
+      break;
+    case 'projects':
+      console.log('clicked on','projects');
+      RenderProjects()
+      break;
+  }
+}
+
